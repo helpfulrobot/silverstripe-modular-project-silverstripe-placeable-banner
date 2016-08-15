535036264cbcb2cab073d57fe8b1ec165fe9475f
Gorrie <gorriecoe@gmail.com>
2016-08-15 21:52:29 +1200

    Added background image

--- code/models/PlaceableBanner.php
+++ code/models/PlaceableBanner.php
@@ -21,7 +21,18 @@ class PlaceableBanner extends DataObject
      * @var array
      */
     private static $has_one = array(
-        'Button' => 'Link',
+        'Background' => 'Image',
+        'Button' => 'Link'
+    );
+
+    /**
+     * Defines summary fields commonly used in table columns
+     * as a quick overview of the data for this dataobject
+     * @var array
+     */
+    private static $summary_fields = array(
+        'Background.CMSThumbnail' => 'Background',
+        'Title' => 'Title'
     );
 
     /**
@@ -34,11 +45,21 @@ class PlaceableBanner extends DataObject
         $fields->addFieldsToTab(
             'Root.Main',
             array(
-                TextField::create('Title'),
-                TextareaField::create('Content'),
+                TextField::create(
+                    'Title',
+                    _t('PlaceableBanner.TITLE', 'Title')
+                ),
+                TextareaField::create(
+                    'Content',
+                    _t('PlaceableBanner.CONTENT', 'Content')
+                ),
+                UploadField::create(
+                    'Background',
+                    _t('PlaceableBanner.BACKGROUND', 'Background')
+                ),
                 LinkField::create(
                     'ButtonID',
-                    'Button'
+                    _t('PlaceableBanner.BUTTON', 'Button')
                 )
             )
         );
